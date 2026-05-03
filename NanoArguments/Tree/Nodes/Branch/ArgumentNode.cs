c6667a0035f31f17c170c5ecbb4365960c910605
Kiber2009 <92222946+Kiber2009@users.noreply.github.com>
2026-05-03 20:08:26 +0300

    Remove generic from ArgumentNode

--- NanoArguments/Tree/Nodes/Branch/ArgumentNode.cs
+++ NanoArguments/Tree/Nodes/Branch/ArgumentNode.cs
@@ -3,7 +3,7 @@ using NanoArguments.Tree.Arguments;
 
 namespace NanoArguments.Tree.Nodes.Branch;
 
-public class ArgumentNode<T>(IArgumentParser<T> parser, string name) : BranchNode
+public class ArgumentNode(IArgumentParser<object> parser, string name) : BranchNode
 {
     public override bool Check(ParsingContext context, uint pos)
     {
@@ -13,7 +13,7 @@ public class ArgumentNode<T>(IArgumentParser<T> parser, string name) : BranchNod
 
     public override Action<ParsingContext> Parse(ParsingContext context, uint pos)
     {
-        T res = parser.Parse(context, context.ParserResult.PositionalArgs[pos]);
+        object res = parser.Parse(context, context.ParserResult.PositionalArgs[pos]);
         context.AddArgument(name, res);
         return base.Parse(context, pos + 1);
     }
