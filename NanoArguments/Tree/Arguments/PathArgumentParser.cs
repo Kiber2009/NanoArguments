196b9c665fa46b51861cc391b2fc5bf4b9699ccb
Kiber2009 <92222946+Kiber2009@users.noreply.github.com>
2026-05-03 18:38:13 +0300

    Add PathArgumentParser

---
+++ NanoArguments/Tree/Arguments/PathArgumentParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace NanoArguments.Tree.Arguments;
+
+public class PathArgumentParser : IArgumentParser<string>
+{
+    private readonly Filter _filter;
+
+    public PathArgumentParser(Filter filter = Filter.Any)
+    {
+        if (!Enum.IsDefined(filter))
+            throw new ArgumentOutOfRangeException(nameof(filter), filter, null);
+        _filter = filter;
+    }
+
+    public bool Check(ParsingContext context, string value)
+    {
+        return _filter switch
+        {
+            Filter.Any => Utils.IsValidPath(value),
+            Filter.NotExists => !Path.Exists(value),
+            Filter.Exists => Path.Exists(value),
+            Filter.ExistsFile => File.Exists(value),
+            Filter.ExistsDirectory => Directory.Exists(value),
+            _ => throw new ArgumentOutOfRangeException(null, _filter, null)
+        };
+    }
+
+    public string Parse(ParsingContext context, string value)
+    {
+        return value;
+    }
+
+    public enum Filter : byte
+    {
+        Any,
+        NotExists,
+        Exists,
+        ExistsFile,
+        ExistsDirectory
+    }
+}
