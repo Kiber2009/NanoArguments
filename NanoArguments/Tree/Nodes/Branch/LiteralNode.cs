9ebae9ca217781654e1b72b101de4aa82b402774
Kiber2009 <92222946+Kiber2009@users.noreply.github.com>
2026-05-10 00:00:53 +0300

    Implement feature

--- NanoArguments/Tree/Nodes/Branch/LiteralNode.cs
+++ NanoArguments/Tree/Nodes/Branch/LiteralNode.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NanoArguments.Tree.Nodes.Branch;
 
-public class LiteralNode(string literal) : BranchNode
+public class LiteralNode(ISet<string> literal) : BranchNode
 {
+    public LiteralNode(string literal) : this(new HashSet<string> { literal }) { }
+
     public override bool Check(ParsingContext context, uint pos)
     {
-        return context.ParserResult.PositionalArgs[pos] == literal;
+        return literal.Contains(context.ParserResult.PositionalArgs[pos]);
     }
 
     public override Action<ParsingContext> Parse(ParsingContext context, uint pos)
